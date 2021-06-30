d3280a8571032e54b4c70ec3978118119c5870b3
NorifumiOgawa <n@ogaworks.com>
2021-06-30 14:52:39 +0900

    提出課題修正1回目

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,6 +4,9 @@
 require 'optparse'
 require 'etc'
 
+COLUMNS = 3 # 3列表示
+PERMISSIONS = { '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }.freeze
+
 def main
   options = ARGV.getopts('a', 'l', 'r')
   file_list = if options['a']
@@ -22,15 +25,13 @@ end
 
 def listing(file_list)
   # -l オプションなしのファイルリスト表示
-  col = 3 # 3列表示
   file_count = file_list.size
-  row = (file_count % col).positive? ? file_count / col + 1 : file_count / col
-  max_values = values_max_length(file_list)
-
+  row = (file_count % COLUMNS).positive? ? file_count / COLUMNS + 1 : file_count / COLUMNS
+  max_lengths = values_max_length(file_list)
   listing_text = ''
   row.times do |i|
-    col.times do |ii|
-      listing_text += mb_ljust(file_list[i + ii * row], max_values[0] + 1) if i + ii * row < file_count
+    COLUMNS.times do |ii|
+      listing_text += mb_ljust(file_list[i + ii * row], max_lengths[:file_name] + 1) if i + ii * row < file_count
     end
     listing_text += "\n"
   end
@@ -41,28 +42,26 @@ def listing_option_l(file_list)
   # -l オプション指定時のファイルリスト表示
   # -rwxr--r--  1 ogaworks  staff   409B  6 22 22:59 ls.rb
   xattr_file_list = extended_attributes(ARGV)
-  max_values = values_max_length(file_list)
+  max_lengths = values_max_length(file_list)
   file_list.each do |file|
     file_path = "#{ARGV[0]}#{file}"
     stat = File.lstat(file_path)
     list_text = "#{file_type(stat.mode)}#{file_permission(stat.mode)}"
     list_text += xattr_file_list&.include?(file) && file_type(stat.mode) == '-' ? '@' : ' '
     list_text += "#{stat.nlink.to_s.rjust(3)} "
-    list_text += "#{Etc.getpwuid(File.lstat(file_path).uid).name.ljust(max_values[2])} #{Etc.getgrgid(File.lstat(file_path).gid).name.rjust(max_values[3] + 1)}"
-    list_text += "#{stat.size.to_s.rjust(max_values[1] + 2)} "
-    list_text += "#{stat.mtime.strftime('%-m %e %H:%M').rjust(11)} "
-    file_name = file
-    list_text += if file_type(stat.mode) == 'l'
-                   "#{file_name} -> #{File.readlink(file_path)}"
-                 else
-                   file_name
-                 end
+    list_text += Etc.getpwuid(File.lstat(file_path).uid).name.ljust(max_lengths[:user_name])
+    list_text += Etc.getgrgid(File.lstat(file_path).gid).name.rjust(max_lengths[:group_name] + 2)
+    list_text += stat.size.to_s.rjust(max_lengths[:file_size] + 2)
+    list_text += stat.mtime.strftime('%-m %e %H:%M').rjust(12)
+    list_text += " #{file}"
+    list_text += " -> #{File.readlink(file_path)}" if file_type(stat.mode) == 'l'
+
     puts list_text
   end
 end
 
 def mb_ljust(file_name, width)
-  char_count = file_name.each_char.map { |c| c.bytesize == 1 ? 1 : 2 }.inject(0, :+)
+  char_count = file_name.each_char.sum { |c| c.bytesize == 1 ? 1 : 2 }
   file_name += ' ' * (width - char_count)
   file_name
 end
@@ -76,12 +75,13 @@ def values_max_length(file_list)
   file_list.each do |file|
     file_path = "#{ARGV[0]}#{file}"
     stat = File.lstat(file_path)
-    file_name_max_length = file.length if file_name_max_length < file.length
+    file_name_length = file.each_char.sum { |c| c.bytesize == 1 ? 1 : 2 }
+    file_name_max_length = file_name_length if file_name_max_length < file_name_length
     file_size_max_length = stat.size if file_size_max_length < stat.size
     user_name_max_length = Etc.getpwuid(File.lstat(file_path).uid).name.length if user_name_max_length < Etc.getpwuid(File.lstat(file_path).uid).name.length
     group_name_max_length = Etc.getgrgid(File.lstat(file_path).gid).name.length if group_name_max_length < Etc.getgrgid(File.lstat(file_path).gid).name.length
   end
-  [file_name_max_length, file_size_max_length.to_s.size, user_name_max_length, group_name_max_length]
+  { file_name: file_name_max_length, file_size: file_size_max_length.to_s.size, user_name: user_name_max_length, group_name: group_name_max_length }
 end
 
 def file_type(mode)
@@ -101,23 +101,9 @@ end
 def file_permission(mode)
   # -l オプション) 表示用ファイルパーミッション文字列を返す
   # ex.) -rwxr--r--
-  permission_numbers = mode.to_s(8).rjust(7, '0')[-3, 3].split('')
-  permission_string = ''
-  permission_numbers.each do |pn|
-    permission_string += to_rwx(pn.to_i)
-  end
-  permission_string
-end
-
-def to_rwx(p_num)
-  # パーミッションを数値(7~1)からrwx形式に変換して返す
-  # 7:rwx, 6:rw-, 5:r-x, 4:r--, 3:-wx, 2:-w-, 1:--x
-  rwx = %w[r w x]
-  permission_bits = p_num.to_s(2).rjust(3, '0').split('') # 7 => 111
-  rwx.map!.with_index do |permission, i|
-    permission_bits[i] == '1' ? permission : '-'
-  end
-  rwx.join
+  permission_numbers = mode.to_s(8)[-3, 3].split('')
+  permission_numbers.map! { |i| PERMISSIONS[i.to_s] }
+  permission_numbers.join
 end
 
 def extended_attributes(argv)
